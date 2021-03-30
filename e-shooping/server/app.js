27706439aa952718e96201dab6dd4c414f4c703c
Chetan Bhatt <chetanbhatt@Chetans-MacBook-Pro.local>
2021-03-30 19:22:45 +0530

    folder structure

---
+++ e-shooping/server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+var cors = require('cors')
+require('dotenv').config();
+
+
+const app = express();
+
+// const postsRoute = require('./routes/posts');
+
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// app.use('/posts', postsRoute);
+
+// Db connect
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('database connected'))
+    .catch(() => console.log('database not connected'));
+
+
+// server is running
+app.listen(process.env.PORT || 4000);
