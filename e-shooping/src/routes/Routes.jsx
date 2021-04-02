c1cdcc2a721821914d05df3b5672c0eb8b73405b
Chetan Bhatt <chetanbhatt@Chetans-MacBook-Pro.local>
2021-04-02 22:50:37 +0530

    all changes

--- e-shooping/src/routes/Routes.jsx
+++ e-shooping/src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
+const MainView = lazy(() => import('../container/main/Main'));
 const SignInView = lazy(() => import('../container/account/SignIn'));
 const SignUpView = lazy(() => import("../container/account/SignUp"));
 const Routes = _ => {
@@ -8,6 +9,7 @@ const Routes = _ => {
             <Suspense
                 fallback={<div className="text-white text-center mt-3">Loading...</div>} >
                 <Switch>
+                    <Route exact path="/" component={MainView} />
                     <Route path="/login" component={SignInView} />
                     <Route path="/register" component={SignUpView} />
                 </Switch>
