0546527f90f4693507786b5e9244495e904825ed
uilegendsteam@gmail.com <uilegendsteam@gmail.com>
2021-04-04 15:11:07 +0530

    chnge in app

--- e-shooping/src/container/main/Main.jsx
+++ e-shooping/src/container/main/Main.jsx
@@ -1,8 +1,27 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { APIURL } from '../../helpers/constrants';
 import { Link } from 'react-router-dom';
 
-const Main = props => {
+const Main = () => {
+
+    const [data, setData] = useState([]);
+    useEffect(async () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(
+                {
+                    page: 1,
+                    pageSize: 10
+                }
+            )
+        };
+        const result = await axios(APIURL.ALLPRODUCTS, requestOptions);
+        setData(result.data.data);
+    });
+
     return (
         <>
             <div className="bg-info bg-gradient p-3 text-center mb-3">
@@ -10,52 +29,21 @@ const Main = props => {
             </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3">
-                        <Link to="/" className="text-decoration-none">
-                            <img
-                                src="../../images/category/male.webp"
-                                className="img-fluid rounded-circle"
-                                alt="..."
-                            />
-                            <div className="text-center h6">Men's Clothing</div>
-                        </Link>
-                    </div>
-                    <div className="col-md-3">
-                        <Link to="/" className="text-decoration-none">
-                            <img
-                                src="../../images/category/female.webp"
-                                className="img-fluid rounded-circle"
-                                alt="..."
-                            />
-                            <div className="text-center h6">Women's Clothing</div>
-                        </Link>
-                    </div>
-                    <div className="col-md-3">
-                        <Link to="/" className="text-decoration-none">
-                            <img
-                                src="../../images/category/smartwatch.webp"
-                                className="img-fluid rounded-circle"
-                                alt="..."
-                            />
-                            <div className="text-center h6">Smartwatch</div>
-                        </Link>
-                    </div>
-                    <div className="col-md-3">
-                        <Link to="/" className="text-decoration-none">
-                            <img
-                                src="../../images/category/footwear.webp"
-                                className="img-fluid rounded-circle"
-                                alt="..."
-                            />
-                            <div className="text-center h6">Footwear</div>
-                        </Link>
-                    </div>
+                    {data.map(item => (
+                        <div className="col-md-3" key={item.productTitle} >
+                            <Link to="/" className="text-decoration-none">
+                                <img
+                                    src={item.image}
+                                    className="img-fluid rounded-circle"
+                                    alt="..."
+                                />
+                                <div className="text-center h6">{item.productTitle}</div>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
     );
 };
-
-
-
 export default Main;
--- e-shooping/src/helpers/constrants.js
+++ e-shooping/src/helpers/constrants.js
@@ -1,5 +1,6 @@
 export const APIURL = {
     API_BASE_URL: 'http://localhost:4000/api/',
     LOGIN: 'auth/login',
-    REGISTER: 'auth/register'
+    REGISTER: 'auth/register',
+    ALLPRODUCTS: 'products/allProducts'
 }
