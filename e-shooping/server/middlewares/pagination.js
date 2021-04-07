b95c2562b401930422fc3dba7c8cbb3d5b959f98
Chetan Bhatt <chetanbhatt@Chetans-MacBook-Pro.local>
2021-04-07 21:20:13 +0530

    all changes

--- e-shooping/server/middlewares/pagination.js
+++ e-shooping/server/middlewares/pagination.js
@@ -4,7 +4,7 @@ exports.pagination = (model) => {
     return async (req, res, next) => {
         const page = parseInt(req.body.page);
         const pageSize = parseInt(req.body.pageSize);
-        const categoryId = req.params.categoryId;
+        const categoryId = req.body.categoryId;
         const startIndex = (page - 1) * pageSize;
         try {
             const productsResult = await model.find({ categoryId: categoryId }).skip(startIndex).limit(pageSize);
@@ -12,6 +12,7 @@ exports.pagination = (model) => {
             if (productsResult) {
                 res.paginatedResult = productsResult;
                 res.totalItemCount = totalItemCount;
+                res.categoryId = categoryId
                 next();
             }
         } catch (e) {
